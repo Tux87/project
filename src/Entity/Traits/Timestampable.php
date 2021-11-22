2c9617425023397dc2ca54477594c988fda024ed
Tux87 <edward.juniper87@gmail.com>
2021-11-22 14:41:43 +0100

    Added Traits and Timestampable trait

--- src/Entity/Traits/Timestampable.php
+++ src/Entity/Traits/Timestampable.php
@@ -1,36 +1,9 @@
-<?php
+<?php 
 
-namespace App\Entity;
+namespace App\Entity\Traits;
 
-use App\Repository\PinRepository;
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * @ORM\Entity(repositoryClass=PinRepository::class)
- * @ORM\Table(name="pins")
- * //Allows the listening of events in the current entity
- * @ORM\HasLifecycleCallbacks
- */
-class Pin
+trait Timestampable
 {
-    use Timestampable;
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $title;
-
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $description;
-
     /**
      * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
      */
@@ -40,35 +13,6 @@ class Pin
      * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
      */
     private $updatedAt;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
 
     public function getCreatedAt(): ?\DateTimeInterface
     {
@@ -107,3 +51,4 @@ class Pin
         $this->setUpdatedAt(new \DateTimeImmutable);
     }
 }
+?>
