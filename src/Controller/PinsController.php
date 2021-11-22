46df249f40f1ff3a0e6da3d5cffb74625f308c9c
Tux87 <edward.juniper87@gmail.com>
2021-11-22 13:37:55 +0100

    Created singularToPlural Twig function

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PinRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
     /**
-     * @Route("", name="app_home")
+     * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(PinRepository $pinRepository): Response
     {
-        return $this->render('pins/index.html.twig');
+        $allPins = $pinRepository->findAll();
+        return $this->render('pins/index.html.twig', compact('allPins'));
     }
 }
