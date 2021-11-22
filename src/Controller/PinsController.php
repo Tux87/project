c7b538f0975d4c13c9382b5e82926e4976b0cc45
Tux87 <edward.juniper87@gmail.com>
2021-11-22 13:55:31 +0100

    Created Pin show page

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,5 +17,13 @@ class PinsController extends AbstractController
     {
         $allPins = $pinRepository->findAll();
         return $this->render('pins/index.html.twig', compact('allPins'));
+    }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="app_pin_show")
+     */
+    public function show(Pin $pin): Response
+    {
+       return $this->render('pins/show.html.twig', compact('pin'));
     }
 }
