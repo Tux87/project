2d510212e72c0bb34fcd150fa46c323580cc70b1
Tux87 <edward.juniper87@gmail.com>
2021-11-23 11:23:43 +0100

    Added create pin function

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -4,14 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Pin;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home", methods={"GET"})
      */
     public function index(PinRepository $pinRepository): Response
     {
@@ -21,7 +25,40 @@ class PinsController extends AbstractController
     }
 
     /**
-     * @Route("/pins/{id<[0-9]+>}", name="app_pin_show")
+     * @Route("/pin/creer", name="app_pin_create", methods={"GET", "POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+       $form = $this->createFormBuilder(new Pin)
+                    ->add('title', TextType::class)
+                    ->add('description', TextareaType::class)
+                    ->getForm()
+        ;
+    
+        $form->handleRequest($request);
+        //If the form is submitted and is valid then:
+        if ($form->isSubmitted() && $form->isValid()){
+            //Add submitted data to $data
+            $data = $form->getData();
+            //Create a new pin
+            $pin = new Pin();
+            //Add title and description
+            $pin->setTitle($data['title']);
+            $pin->setDescription($data['description']);
+            //Add to DB
+            $em->persist($pin);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+       return $this->render('pins/create.html.twig', [
+           'form' => $form->createView(),
+       ]);
+    }
+
+    /**
+     * @Route("/pin/{id<[0-9]+>}", name="app_pin_show", methods="GET")
      */
     public function show(Pin $pin): Response
     {
