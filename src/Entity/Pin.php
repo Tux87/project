3b0e593a6daa9062e28f3274b266eb5a70ec1d90
Tux87 <edward.juniper87@gmail.com>
2021-11-24 12:55:36 +0100

    Fixed bug where entity needed ?string on setter

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -46,7 +46,7 @@ class Pin
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    public function setTitle(?string $title): self
     {
         $this->title = $title;
 
